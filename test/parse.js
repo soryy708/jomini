df47c84a445800a10db08e979e4cd3e17d567bfe
Nick Babcock <nbabcock19@hotmail.com>
2015-02-14 08:31:29 -0500

    Convert tests to handrolled

--- test/parse.js
+++ test/parse.js
@@ -1,127 +1,129 @@
 var parse = require('../lib/jomini').parse;
 var expect = require('chai').expect;
 
+function check(str, obj, cb) {
+  parse(str, function(actual) {
+    expect(actual).to.deep.equal(obj);
+  });
+}
+
 describe('parse', function() {
-  it('should handle the simple parse case', function() {
-    expect(parse('foo=bar')).to.deep.equal({foo: 'bar'});
+  it('should handle the simple parse case', function(done) {
+    check('foo=bar', {foo: 'bar'}, done);
   });
 
-  it('should handle the simple header case', function() {
-    expect(parse('EU4txt\nfoo=bar')).to.deep.equal({foo: 'bar'});
+  it('should handle the simple header case', function(done) {
+    check('EU4txt\nfoo=bar', {foo: 'bar'}, done);
   });
 
-  it('should handle empty quoted strings', function() {
-    expect(parse('foo=""')).to.deep.equal({foo: ''});
+  it('should handle empty quoted strings', function(done) {
+    check('foo=""', {foo: ''}, done);
   });
 
-  it('should handle whitespace', function() {
-    expect(parse('\tfoo = bar ')).to.deep.equal({'foo':'bar'});
+  it('should handle whitespace', function(done) {
+    check('\tfoo = bar ', {'foo':'bar'}, done);
   });
 
-  it('should handle the simple quoted case', function() {
-    expect(parse('foo="bar"')).to.deep.equal({'foo':'bar'});
+  it('should handle the simple quoted case', function(done) {
+    check('foo="bar"', {'foo':'bar'}, done);
   });
 
-  it('should handle string list accumulation', function() {
-    expect(parse('foo=bar\nfoo=qux')).to.deep.equal({'foo':['bar', 'qux']});
+  it('should handle string list accumulation', function(done) {
+    check('foo=bar\nfoo=qux', {'foo':['bar', 'qux']}, done);
   });
 
-  it('should handle string list accumulation long', function() {
-    expect(parse('foo=bar\nfoo=qux\nfoo=baz')).to.deep.
-        equal({'foo':['bar', 'qux', 'baz']});
+  it('should handle string list accumulation long', function(done) {
+    check('foo=bar\nfoo=qux\nfoo=baz', {'foo':['bar', 'qux', 'baz']}, done);
   });
 
-  it('should handle quoted string list accumulation', function() {
-    expect(parse('foo="bar"\nfoo="qux"')).to.deep.
-        equal({'foo':['bar', 'qux']});
+  it('should handle quoted string list accumulation', function(done) {
+    check('foo="bar"\nfoo="qux"', {'foo':['bar', 'qux']}, done);
   });
 
-  it('should handle boolen', function() {
-    expect(parse('foo=yes')).to.deep.equal({'foo': true});
+  it('should handle boolen', function(done) {
+    check('foo=yes', {'foo': true}, done);
   });
 
-  it('should handle boolen list', function() {
-    expect(parse('foo={yes no}')).to.deep.equal({'foo': [true, false]});
+  it('should handle boolen list', function(done) {
+    check('foo={yes no}', {'foo': [true, false]}, done);
   });
 
-  it('should handle whole numbers', function() {
-    expect(parse('foo=1')).to.deep.equal({'foo': 1});
+  it('should handle whole numbers', function(done) {
+    check('foo=1', {'foo': 1}, done);
   });
 
-  it('should handle zero', function() {
-    expect(parse('foo=0')).to.deep.equal({'foo': 0});
+  it('should handle zero', function(done) {
+    check('foo=0', {'foo': 0}, done);
   });
 
-  it('should handle negative whole numbers', function() {
-    expect(parse('foo=-1')).to.deep.equal({'foo': -1});
+  it('should handle negative whole numbers', function(done) {
+    check('foo=-1', {'foo': -1}, done);
   });
 
-  it('should handle decimal number', function() {
-    expect(parse('foo=1.23')).to.deep.equal({'foo': 1.23});
+  it('should handle decimal number', function(done) {
+    check('foo=1.23', {'foo': 1.23}, done);
   });
 
-  it('should handle negative decimal number', function() {
-    expect(parse('foo=-1.23')).to.deep.equal({'foo': -1.23});
+  it('should handle negative decimal number', function(done) {
+    check('foo=-1.23', {'foo': -1.23}, done);
   });
 
-  it('should handle number list accumulation', function() {
-    expect(parse('foo=1\nfoo=-1.23')).to.deep.equal({'foo':[1, -1.23]});
+  it('should handle number list accumulation', function(done) {
+    check('foo=1\nfoo=-1.23', {'foo':[1, -1.23]}, done);
   });
 
-  it('should handle dates', function() {
-    expect(parse('date=1821.1.1')).to.deep.
-        equal({'date': new Date(Date.UTC(1821, 0, 1))});
+  it('should handle dates', function(done) {
+    check('date=1821.1.1', {'date': new Date(Date.UTC(1821, 0, 1))}, done);
   });
 
-/*  it('should deceptive dates', function() {
-    expect(parse('date=1821.a.1')).to.deep.equal({date': '1821.a.1'});
+/*  it('should deceptive dates', function(done) {
+    check('date=1821.a.1', {date': '1821.a.1'}, done);
   });*/
 
-  it('should handle quoted dates', function() {
-    expect(parse('date="1821.1.1"')).to.deep.
-        equal({'date': new Date(Date.UTC(1821, 0, 1))});
+  it('should handle quoted dates', function(done) {
+    check('date="1821.1.1"', {'date': new Date(Date.UTC(1821, 0, 1))}, done);
   });
 
-  it('should handle accumulated dates', function() {
-    expect(parse('date="1821.1.1"\ndate=1821.2.1')).to.deep.equal(
-      {'date':[new Date(Date.UTC(1821, 0, 1)), new Date(Date.UTC(1821, 1, 1))]}
-    );
+  it('should handle accumulated dates', function(done) {
+    check('date="1821.1.1"\ndate=1821.2.1',
+      {'date':[
+        new Date(Date.UTC(1821, 0, 1)), new Date(Date.UTC(1821, 1, 1))
+      ]}, done);
   });
 
-  it('should handle numbers as identifiers', function() {
-    expect(parse('158=10')).to.deep.equal({'158': 10});
+  it('should handle numbers as identifiers', function(done) {
+    check('158=10', {'158': 10}, done);
   });
 
-  it('should handle periods in identifiers', function() {
-    expect(parse('flavor_tur.8=yes')).to.deep.equal({'flavor_tur.8': true});
+  it('should handle periods in identifiers', function(done) {
+    check('flavor_tur.8=yes', {'flavor_tur.8': true}, done);
   });
 
-  it('should handle empty objects for dates', function() {
-    expect(parse('1920.1.1={}')).to.deep.equal({'1920.1.1': {}});
+  it('should handle empty objects for dates', function(done) {
+    check('1920.1.1={}', {'1920.1.1': {}}, done);
   });
 
-  it('should handle consecutive strings', function() {
-    expect(parse('foo = { bar baz }')).to.deep.equal({'foo': ['bar', 'baz']});
+  it('should handle consecutive strings', function(done) {
+    check('foo = { bar baz }', {'foo': ['bar', 'baz']}, done);
   });
 
-  it('should handle consecutive strings no space', function() {
-    expect(parse('foo={bar baz}')).to.deep.equal({'foo': ['bar', 'baz']});
+  it('should handle consecutive strings no space', function(done) {
+    check('foo={bar baz}', {'foo': ['bar', 'baz']}, done);
   });
 
-  it('should handle consecutive quoted strings', function() {
-    expect(parse('foo = { "bar" "baz" }')).to.deep.
-        equal({'foo': ['bar', 'baz']});
+  it('should handle consecutive quoted strings', function(done) {
+    check('foo = { "bar" "baz" }', {'foo': ['bar', 'baz']}, done);
   });
 
-  it('should handle empty object', function() {
-    expect(parse('foo = {}')).to.deep.equal({'foo': {}});
+  it('should handle empty object', function(done) {
+    check('foo = {}', {'foo': {}}, done);
   });
 
-  it('should handle space empty object', function() {
-    expect(parse('foo = { }')).to.deep.equal({'foo': {}});
+  it('should handle space empty object', function(done) {
+    check('foo = { }', {'foo': {}}, done);
   });
 
-  it('should handle the object after empty object', function() {
+  it('should handle the object after empty object', function(done) {
     var obj = {
       foo: {},
       catholic: {
@@ -129,10 +131,10 @@ describe('parse', function() {
       }
     };
 
-    expect(parse('foo={} catholic={defender="me"}')).to.deep.equal(obj);
+    check('foo={} catholic={defender="me"}', obj, done);
   });
 
-  it('should handle the object after empty object nested', function() {
+  it('should handle the object after empty object nested', function(done) {
     var obj = {
       religion: {
         foo: {},
@@ -142,16 +144,14 @@ describe('parse', function() {
       }
     };
 
-    expect(parse('religion={foo={} catholic={defender="me"}}')).to.deep.
-        equal(obj);
+    check('religion={foo={} catholic={defender="me"}}', obj, done);
   });
 
-  it('should ignore empty objects with no identifier at end', function() {
-    expect(parse('foo={bar=val {}}  { } me=you')).to.deep.
-        equal({foo: {bar: 'val'}, me: 'you'});
+  it('should ignore empty objects with no identifier at end', function(done) {
+    check('foo={bar=val {}}  { } me=you', {foo: {bar: 'val'}, me: 'you'}, done);
   });
 
-  it('should understand a list of objects', function() {
+  it('should understand a list of objects', function(done) {
     var str = 'attachments={ { id=258579 type=4713 } ' +
       ' { id=258722 type=4713 } }';
     var obj = {
@@ -164,10 +164,10 @@ describe('parse', function() {
       }]
     };
 
-    expect(parse(str)).to.deep.equal(obj);
+    check(str, obj, done);
   });
 
-  it('should parse minimal spacing for objects', function() {
+  it('should parse minimal spacing for objects', function(done) {
     var str = 'nation={ship={name="ship1"} ship={name="ship2"}}';
     var obj = {
       nation: {
@@ -175,10 +175,10 @@ describe('parse', function() {
       }
     };
 
-    expect(parse(str)).to.deep.equal(obj);
+    check(str, obj, done);
   });
 
-  it('should understand a simple EU4 header', function() {
+  it('should understand a simple EU4 header', function(done) {
     var str = 'date=1640.7.1\r\nplayer="FRA"\r\nsavegame_version=' +
       '\r\n{\r\n\tfirst=1\r\n\tsecond=9\r\n\tthird=2\r\n\tforth=0\r\n}';
     var obj = {
@@ -191,10 +191,10 @@ describe('parse', function() {
         forth: 0
       }
     };
-    expect(parse(str)).to.deep.equal(obj);
+    check(str, obj, done);
   });
 
-  it('should understand EU4 gameplay settings', function() {
+  it('should understand EU4 gameplay settings', function(done) {
     var str = 'gameplaysettings=\r\n{\r\n\tsetgameplayoptions=' +
       '\r\n\t{\r\n\t\t1 1 2 0 1 0 0 0 1 1 1 1 \r\n\t}\r\n}';
     var obj = {
@@ -202,10 +202,10 @@ describe('parse', function() {
         setgameplayoptions: [1, 1, 2, 0, 1, 0, 0, 0, 1, 1, 1, 1]
       }
     };
-    expect(parse(str)).to.deep.equal(obj);
+    check(str, obj, done);
   });
 
-  it('should parse multiple objects accumulated', function() {
+  it('should parse multiple objects accumulated', function(done) {
     var str = 'army=\r\n{\r\n\tname="1st army"\r\n\tunit={\r\n\t\t' +
       'name="1st unit"\r\n\t}\r\n}\r\narmy=\r\n{\r\n\tname="2nd army"' +
       '\r\n\tunit={\r\n\t\tname="1st unit"\r\n\t}\r\n\tunit={\r\n\t\t' +
@@ -227,10 +227,10 @@ describe('parse', function() {
       }]
     };
 
-    expect(parse(str)).to.deep.equal(obj);
+    check(str, obj, done);
   });
 
-  it('should handle back to backs', function() {
+  it('should handle back to backs', function(done) {
     var str1 = 'POR={type=0 max_demand=2.049 t_in=49.697 t_from=\r\n' +
       '{ C00=5.421 C18=44.276 } }';
     var str2 = 'SPA= { type=0 val=3.037 max_pow=1.447 max_demand=2.099 ' +
@@ -254,45 +254,43 @@ describe('parse', function() {
       }
     };
 
-    expect(parse(str1 + str2)).to.deep.equal(expected);
+    check(str1 + str2, expected, done);
   });
 
-  it('should handle dates as identifiers', function() {
-    expect(parse('1480.1.1=yes')).to.deep.equal({'1480.1.1': true});
+  it('should handle dates as identifiers', function(done) {
+    check('1480.1.1=yes', {'1480.1.1': true}, done);
   });
 
-  it('should handle consecutive numbers', function() {
-    expect(parse('foo = { 1 -1.23 }')).to.deep.equal({'foo': [1, -1.23]});
+  it('should handle consecutive numbers', function(done) {
+    check('foo = { 1 -1.23 }', {'foo': [1, -1.23]}, done);
   });
 
-  it('should handle consecutive dates', function() {
-    expect(parse('foo = { 1821.1.1 1821.2.1 }')).to.deep.equal({'foo':
-      [new Date(Date.UTC(1821, 0, 1)), new Date(Date.UTC(1821, 1, 1))]});
+  it('should handle consecutive dates', function(done) {
+    check('foo = { 1821.1.1 1821.2.1 }', {'foo':
+      [new Date(Date.UTC(1821, 0, 1)), new Date(Date.UTC(1821, 1, 1))]}, done);
   });
 
-  it('should understand comments mean skip line', function() {
-    expect(parse('# boo\r\n# baa\r\nfoo=a\r\n# bee')).to.deep.
-        equal({'foo': 'a'});
+  it('should understand comments mean skip line', function(done) {
+    check('# boo\r\n# baa\r\nfoo=a\r\n# bee', {'foo': 'a'}, done);
   });
 
-  it('should understand simple objects', function() {
-    expect(parse('foo={bar=val}')).to.deep.equal({'foo': {'bar': 'val'}});
+  it('should understand simple objects', function(done) {
+    check('foo={bar=val}', {'foo': {'bar': 'val'}}, done);
   });
 
-  it('should understand nested list objects', function() {
-    expect(parse('foo={bar={val}}')).to.deep.equal({'foo': {'bar': ['val']}});
+  it('should understand nested list objects', function(done) {
+    check('foo={bar={val}}', {'foo': {'bar': ['val']}}, done);
   });
 
-  it('should understand objects with start spaces', function() {
-    expect(parse('foo= { bar=val}')).to.deep.equal({'foo': {'bar': 'val'}});
+  it('should understand objects with start spaces', function(done) {
+    check('foo= { bar=val}', {'foo': {'bar': 'val'}}, done);
   });
 
-  it('should understand objects with end spaces', function() {
-    expect(parse('foo={bar=val }')).to.deep.equal({'foo': {'bar': 'val'}});
+  it('should understand objects with end spaces', function(done) {
+    check('foo={bar=val }', {'foo': {'bar': 'val'}}, done);
   });
 
-  it('should ignore empty objects with no identifier', function() {
-    expect(parse('foo={bar=val} {} { } me=you')).to.deep.
-        equal({foo: {bar: 'val'}, me: 'you'});
+  it('should ignore empty objects with no identifier', function(done) {
+    check('foo={bar=val} {} { } me=you', {foo: {bar: 'val'}, me: 'you'}, done);
   });
 });
